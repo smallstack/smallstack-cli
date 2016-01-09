0bc8380a321b14da83306219c562e5d721c0cd49
Maximilian Friedmann <maximilian.friedmann@gmail.com>
2016-01-09 01:41:20 +0100

    and some more

--- smallstack.js
+++ smallstack.js
@@ -12,19 +12,27 @@ var _ = require("underscore");
 // show a nice logo
 logo();
 
-// check if project is available
-if (!fs.existsSync("smallstack.json") && !_.contains(["create", "--help", "-h", "-v", "--version"], process.argv[2].toLowerCase())) {
-    console.log("No smallstack project found! Why don't you create one : ");
-    console.log(" ");
-    console.log("      smallstack create <name>");
-    process.exit(0);
-}
-
 // parse command line options
 commander.version(config.packageJSON.version);
 commander.usage("command [options]");
 
 commander.command("create <name>").action(create);
 commander.command("generate").action(generate);
+
+
+// no command given?
+if (!process.argv.slice(2).length) {
+    commander.outputHelp();
+    process.exit(0);
+}
+
+// check if project is available
+if (!fs.existsSync("smallstack.json") && (process.argv[2] === undefined || !_.contains(["create", "--help", "-h", "-v", "--version"], process.argv[2].toLowerCase()))) {
+    console.log("No smallstack project found! Why don't you create one : ");
+    console.log(" ");
+    console.log("      smallstack create <name>");
+    process.exit(0);
+}
+
 
 commander.parse(process.argv);
