49ea3337c49a47448f710b43c2d2dbee25a7be00
Maximilian Friedmann <maximilian.friedmann@gmail.com>
2016-04-01 18:00:28 +0200

    onConnectionReady callbacks

--- src/resources/smallstack/interfaces/DataBridge.ts
+++ src/resources/smallstack/interfaces/DataBridge.ts
@@ -1,10 +1,13 @@
 
 interface DataBridge {
+    // connection related
+    onConnectionReady(fn: () => void);
+
     // collections
     subscribe(name: string, parameters: any, options: any, callback: (error: Error, subscribed: boolean) => void): void;
     getCountForQuery(queryName: string, parameters: any, callback: (error: Error, count: number) => void): void;
     getCollectionByName(name: string): any;
-    
+
     // methods
     call(methodName: string, parameters: any, callback: (error: Error, response: any) => void): void;
 
@@ -16,7 +19,8 @@ interface DataBridge {
     loginWithTwitter(callbackFn: (error: Error, success?: boolean) => void): void;
     registerWithPassword(email: string, password: string, callbackFn: (error: Error, success?: boolean) => void): void;
     afterLogin(callbackFn: () => void): void;
-    
+    logout(callbackFn: (error: Error, success?: boolean) => void): void;
+
     // baseUrl
     setBaseUrl(url: string): void;
     getAbsoluteUrl(path: string): string;
