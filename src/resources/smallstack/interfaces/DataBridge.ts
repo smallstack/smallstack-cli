ecd225d41a15b355c62eab8a0a2580c8017c63de
Maximilian Friedmann <maximilian.friedmann@gmail.com>
2016-04-12 07:53:11 +0200

    adding avatarurl to databridge

--- src/resources/smallstack/interfaces/DataBridge.ts
+++ src/resources/smallstack/interfaces/DataBridge.ts
@@ -21,6 +21,7 @@ interface DataBridge {
     registerWithPassword(email: string, password: string, callbackFn: (error: Error, success?: boolean) => void): void;
     afterLogin(callbackFn: () => void): void;
     logout(callbackFn: (error: Error, success?: boolean) => void): void;
+    getAvatarUrlForUser(user: any): string;
 
     // baseUrl
     setBaseUrl(url: string): void;
