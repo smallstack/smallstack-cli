02935372194af2fd6f19a0ee8bd3f4aa57b17765
Maximilian Friedmann <maximilian.friedmann@gmail.com>
2016-10-26 08:50:52 +0200

    fixing create command

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,9 +1,9 @@
-module.exports = function (parameters) {
+module.exports = function (parameters, done) {
 
     var fs = require("fs-extra");
     var path = require("path");
     var _ = require("underscore");
-    var exec = require('child_process').exec;
+    var exec = require('../functions/exec');
 
     var projectName = parameters.name;
 
@@ -18,51 +18,33 @@ module.exports = function (parameters) {
 
     var projectVersion = "0.1.0";
 
-    function prepareFiles() {
+    console.log("Preparing files...");
 
-        console.log("Preparing files...");
+    // create package.json
+    var packageJSONContent = fs.readJSONSync(__dirname + "/../resources/templates/project/package.json");
+    packageJSONContent.name = projectName;
+    packageJSONContent.version = projectVersion;
+    packageJSONContent.smallstack = {};
+    fs.writeJSONSync(path.join(directory, "package.json"), packageJSONContent);
 
-        // create package.json
-        var packageJSONContent = fs.readJSONSync(__dirname + "/../resources/templates/project/package.json");
-        packageJSONContent.name = projectName;
-        packageJSONContent.version = projectVersion;
-        packageJSONContent.smallstack = {};
-        fs.writeJSONSync(path.join(directory, "package.json"), packageJSONContent);
+    // create default .gitignore
+    fs.copySync(__dirname + "/../resources/templates/project/default.gitignore", path.join(directory, ".gitignore"));
+    console.log(' |-- Done\n');
 
-        // create default .gitignore
-        fs.copySync(__dirname + "/../resources/templates/project/default.gitignore", path.join(directory, ".gitignore"));
-        console.log(' |-- Done\n');
-        createMeteorApplication();
+    // create meteor application
+    var appDirectory = path.join(directory, 'meteor');
+    console.log("Creating Meteor application in", appDirectory);
+    if (fs.existsSync(appDirectory + "/.meteor/packages")) {
+        console.log(" |-- Meteor Application exists, skipping code generation!");
+        done();
     }
-
-
-    function createMeteorApplication() {
-        // create meteor application
-        var appDirectory = path.join(directory, 'meteor');
-        console.log("Creating Meteor application in", appDirectory);
-        if (fs.existsSync(appDirectory + "/.meteor/packages"))
-            console.log(" |-- Meteor Application exists, skipping code generation!");
-        else {
-            var process = exec("meteor create app", {
-                cwd: directory
-            });
-            process.stdout.on('data', function (data) {
-                var lines = data.split("\n");
-                _.each(lines, function (line) {
-                    console.log(' |-- ' + line);
-                })
-            });
-            process.stderr.on('data', function (data) {
-                console.error(' |-- ' + data);
-                throw new Error("Aborting since meteor application could not be created!");
-            });
-            process.on('close', function (code) {
+    else {
+        exec("meteor create app", {
+            cwd: directory,
+            finished: function () {
                 fs.renameSync(path.join(directory, "app"), path.join(directory, "meteor"));
-                console.log(' |-- Done\n');
-            });
-        }
+                done();
+            }
+        });
     }
-
-    // start the chain
-    prepareFiles();
 }
