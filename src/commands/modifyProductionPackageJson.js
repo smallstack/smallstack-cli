fce2f3159f6524bf14d08f23561291e29c178045
Maximilian Friedmann <max@smallstack.io>
2017-04-02 11:30:17 +0200

    using joint method

--- src/commands/modifyProductionPackageJson.js
+++ src/commands/modifyProductionPackageJson.js
@@ -2,22 +2,11 @@ var path = require("path");
 var _ = require("underscore");
 var fs = require("fs-extra");
 var config = require("../config");
-
+var modifyProductionPackageJson = require("../functions/modifyProductionPackageJson");
 
 module.exports = function (parameters, done) {
     if (parameters && parameters.file)
         modifyProductionPackageJson(path.resolve(config.rootDirectory, parameters.file));
     else
         modifyProductionPackageJson(path.resolve(config.rootDirectory, "package.json"));
-}
-
-
-function modifyProductionPackageJson(file) {
-    var content = require(file);
-    delete content.devDependencies;
-    delete content.dependencies;
-    delete content.scripts;
-    content.main = content.main.replace("./dist/bundle/", "./")
-    content.types = content.types.replace("./dist/bundle/", "./")
-    fs.writeJSONSync(file, content);
 }
