ee6254210f90d2683bc921c3154d847c14f8b2e5
Maximilian Friedmann <max@smallstack.io>
2017-07-03 10:23:08 +0200

    adding branch to uploadname

--- src/commands/upload.js
+++ src/commands/upload.js
@@ -8,16 +8,22 @@ module.exports = function (parameters, done) {
     var now = new Date();
     var dateString = dateFormat(now, "yyyy-mm-dd-HH-MM-ss");
 
+    var version = config.version;
+    var state = gitState.checkSync(config.rootDirectory);
+    if (state !== undefined && state.branch !== undefined) {
+        version += "-" + state.branch;
+        version = version.replace(new RegExp("/", "gi"), "_");
+    }
     var region = parameters.region || "eu-central-1";
     var bucketName = parameters.bucket || "smallstack-bundles";
-    var uploadName = parameters.filename || "smallstack/" + config.name + "/" + config.version + "-" + dateString + ".tar.gz";
+    var uploadName = parameters.filename || "smallstack/" + config.name + "/" + version + "-" + dateString + ".tar.gz";
 
     console.log("Target File Name : " + uploadName);
     console.log("AWS Region :       " + region);
     console.log("Bucket :           " + bucketName);
     console.log(" ");
-    console.log("Uploading file...");
 
+    console.log("Uploading file...");
 
     AWS.config.region = region;
 
