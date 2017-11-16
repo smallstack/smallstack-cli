b4e2cccb7852909e0b83c74a51608aa847c44e0a
Maximilian Friedmann <max@smallstack.io>
2017-11-16 21:23:12 +0100

    support cascading of commands

--- src/commands/cloud.ts
+++ src/commands/cloud.ts
@@ -7,80 +7,77 @@ export async function cloud(parameters) {
 
     const dockerCloudService = new DockerCloudService();
 
+    // this service instance can be used for sharing between commands
+    let currentService: IDockerCloudService;
+
     if (parameters.listContainers) {
         const containers = await (dockerCloudService.getContainers(_.omit(parameters, "listContainers")));
         console.log(containers.objects);
-        return;
     }
 
     if (parameters.listServices) {
         const services = await (dockerCloudService.getServices(_.omit(parameters, "listServices")));
         console.log(services.objects);
-        return;
     }
 
     if (parameters.listStacks) {
         const services = await (dockerCloudService.getStacks(_.omit(parameters, "listStacks")));
         console.log(services.objects);
-        return;
     }
 
     if (parameters.registerExternalRepository) {
         const response = await (dockerCloudService.registerExternalRepository(_.omit(parameters, "registerExternalRepository")));
         console.log("Successfully added " + response[0].name + " to docker cloud registry!");
-        return;
     }
 
     if (parameters.createService) {
-        const service = await (dockerCloudService.createService(_.omit(parameters, "createService")));
+        currentService = await (dockerCloudService.createService(_.omit(parameters, "createService")));
         console.log("Successfully created service!");
-        console.log("  |- name:       " + service.name);
-        console.log("  |- uuid:       " + service.uuid);
-        console.log("  |- image:      " + service.image_tag);
-        console.log("  |- public dns: " + service.public_dns);
-        return;
+        console.log("  |- name:       " + currentService.name);
+        console.log("  |- uuid:       " + currentService.uuid);
+        console.log("  |- image:      " + currentService.image_tag);
+        console.log("  |- public dns: " + currentService.public_dns);
     }
 
     if (parameters.startService) {
-        let service: IDockerCloudService;
         try {
-            service = await dockerCloudService.getService(_.extend(_.omit(parameters, "startService"), { state: "Not running" }));
+            currentService = await dockerCloudService.getService(_.extend(_.omit(parameters, "startService"), { state: "Not running" }));
         } catch (e) { }
-        if (!service) {
+        if (!currentService) {
             try {
-                service = await (dockerCloudService.getService(_.extend(_.omit(parameters, "startService"), { state: "Stopped" })));
+                currentService = await (dockerCloudService.getService(_.extend(_.omit(parameters, "startService"), { state: "Stopped" })));
             } catch (e) { }
         }
-        if (!service)
+        if (!currentService)
             throw new Error("Couldn't find a non running or stopped service for the provided parameters!");
-        const uuid = service.uuid;
+        const uuid = currentService.uuid;
         console.log("Starting Service with UUID " + uuid);
         await (dockerCloudService.sendServiceCommand(uuid, "start"));
         console.log("Service start command successfully sent!");
-        return;
     }
 
     if (parameters.stopService) {
         if (parameters.name) {
-            const service = await (dockerCloudService.getServices({ name: parameters.name, state: "Running" }));
-            if (service.meta.total_count !== 1)
+            const services = await (dockerCloudService.getServices({ name: parameters.name, state: "Running" }));
+            if (services.meta.total_count !== 1)
                 throw new Error("Couldn't find a running service with the name '" + parameters.name + "'!");
-            const uuid = service.objects[0].uuid;
+            currentService = services.objects[0];
+            const uuid = currentService.uuid;
             console.log("Stopping Service with UUID " + uuid);
             await (dockerCloudService.sendServiceCommand(uuid, "stop"));
             console.log("Service stop command successfully sent!");
         }
         else
             throw new Error("Please provide a service name via --name parameter!");
-        return;
     }
 
     if (parameters.terminateService) {
         if (parameters.name) {
-            const service = await (dockerCloudService.getServices({ name: parameters.name }));
-            if (service.meta.total_count !== 1)
+            const services = await (dockerCloudService.getServices({ name: parameters.name }));
+            if (services.meta.total_count !== 1)
                 throw new Error("Couldn't find a service with the name '" + parameters.name + "'!");
-            const uuid = service.objects[0].uuid;
+            currentService = services.objects[0];
+            const uuid: string = currentService.uuid;
             console.log("Terminating Service with UUID " + uuid);
             await (dockerCloudService.sendServiceCommand(uuid, "terminate"));
             console.log("Service terminate command successfully sent!");
@@ -91,7 +88,6 @@ export async function cloud(parameters) {
         }
         else
             throw new Error("Please provide a service name via --name parameter!");
-        return;
     }
 
     if (parameters.linkServices) {
@@ -126,7 +122,6 @@ export async function cloud(parameters) {
 
         await (dockerCloudService.linkServices(fromService.uuid, toService.uuid, parameters.name));
         console.log("Service linking was successful!");
-        return;
     }
 
     if (parameters.unlinkServices) {
@@ -143,7 +138,6 @@ export async function cloud(parameters) {
 
         await (dockerCloudService.unlinkServices(fromUUIDs.objects[0].uuid, toUUIDs.objects[0].uuid));
         console.log("Service unlinking was successful!");
-        return;
     }
 
     if (parameters.waitForState) {
@@ -156,12 +150,9 @@ export async function cloud(parameters) {
         await dockerCloudService.waitForState(parameters);
         const durationString: string = moment((new Date().getTime() - waitStart.getTime())).format('mm:ss.SSS');
         console.log("Service is now in state " + parameters.state + " after waiting for " + durationString);
-
-        return;
     }
 
     if (parameters.httpReachableTest) {
         await dockerCloudService.waitForURL(parameters.url);
-        return;
     }
 }
